63ec2b532fcb4de8eef35b5cf40aab28c2b46858
Jai0401 <21cs3025@rgipt.ac.in>
2024-06-21 22:06:08 +0530

    Improvements

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,11 @@ import re
 import requests
 import google.generativeai as genai
 from flask_cors import CORS
-import youtube_dl
+import os
+from bs4 import BeautifulSoup
 
 
-genai.configure(api_key="AIzaSyBdcA82rIdIhyfvb9yxN1qz1-6xtC5TfH4")
+genai.configure(api_key= os.getenv('GOOGLE_API_KEY'))
 
 app = Flask(__name__)
 CORS(app)
@@ -36,13 +37,10 @@ def get_youtube_transcription(video_link):
 
 def get_video_title(video_url):
     try:
-        ydl_opts = {
-            'verbose': True,  # Enable verbose output
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(video_url, download=False)
-            video_title = info_dict.get('title')
-            return video_title
+        req = requests.get(video_url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        title = soup.find('title').text
+        return title
     except Exception as e:
         return str(e)
 
@@ -58,32 +56,7 @@ input_prompt_template = """
 Transcription Text:
 {transcription_text}
 
-Based on the provided transcription, create a comprehensive summary of the video content. The summary should follow the structure below and capture all the essential information without omitting any critical points:
-
-1. **Title**: Provide a brief title for the summary.
-2. **Introduction**: Summarize the main introduction points made by the speaker.
-3. **Key Points**:
-    - **Point 1**: Describe the first key point or section of the video.
-    - **Point 2**: Describe the second key point or section of the video.
-    - **Point 3**: Describe the third key point or section of the video.
-    - (Add more points as necessary)
-4. **Examples**: Provide any examples the speaker used to illustrate their points.
-5. **Benefits and Impact**: Summarize the benefits and impact discussed in the video.
-6. **Conclusion**: Provide the concluding remarks and the main takeaway from the video.
-
-Ensure each section is detailed and clearly written.
-
-Response format:
-**Title**: [Title]
-**Introduction**: [Introduction]
-**Key Points**:
-    - **Point 1**: [Details of Point 1]
-    - **Point 2**: [Details of Point 2]
-    - **Point 3**: [Details of Point 3]
-    - (Add more points as necessary)
-**Examples**: [Examples]
-**Benefits and Impact**: [Benefits and Impact]
-**Conclusion**: [Conclusion]
+Based on the provided transcription, create a comprehensive summary of the video content in a paragraph format:
 
 """
 
@@ -92,13 +65,15 @@ def process_youtube_link():
     if request.method == 'POST':
         data = request.get_json()
         youtube_link = data.get('youtube_link')
+        if(youtube_link is None):
+            return jsonify({'error': 'YouTube link not provided.'}), 400
 
         if youtube_link:
             transcription_text = get_youtube_transcription(youtube_link)
             input_prompt = input_prompt_template.format(transcription_text=transcription_text)
             summary = get_gemini_response(input_prompt)
-            # title = get_video_title(youtube_link)
-            return jsonify({'video_summary': summary})
+            title = get_video_title(youtube_link)
+            return jsonify({'title': title.rsplit(' ', 2)[0],'summary': summary})
         else:
             return jsonify({'error': 'YouTube link not provided.'}), 400
     else:
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
 import { useState } from 'react'
 import axios from 'axios';
-import VideoSummary from './components/Summary';
 
 function App() {
   const [url, setUrl] = useState<string>('')
   const [summary, setSummary] = useState<string | null>(null);
+  const [title, setTitle] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
   const handleSubmit = () => {
@@ -14,23 +14,34 @@ function App() {
     axios.post('http://127.0.0.1:5000/get_summary', {youtube_link: url})
     .then(res => {
       console.log(res.data);
-      setSummary(res.data.video_summary);
+      setSummary(res.data.summary);
+      setTitle(res.data.title);
+      setLoading(false);
+    })
+    .catch(err => {
+      alert(err.response.data.error);
       setLoading(false);
     })
   }
 
   return (
-    <>
-      <h1>BriefTube</h1>
-      <p >YouTube video summariser</p>
-      <input type="text" value={url} placeholder="Paste a YouTube URL" onChange={(e)=>setUrl(e.target.value)}/>
-      <button onClick={handleSubmit}>Submit</button>
+    <div className='m-6'>
+      <h1 className='text-center text-2xl'>BriefTube</h1>
+      <p className='text-center text-lg p-4'>YouTube video summariser</p>
+      <div className='flex justify-center p-4'>
+      <input type="text" value={url} placeholder="Paste a YouTube URL" onChange={(e)=>setUrl(e.target.value)}
+      className='w-96 h-10 mr-2 border-black border-2 align-middle rounded-md'/>
+      <button onClick={handleSubmit} className="border-black border-2 p-2 h-10 rounded-md hover:text-white hover:bg-black">Submit</button>
+      </div>
       {loading ?<><br/>Loading...</> : summary ? (
-        <>
-          <VideoSummary summary={summary}/>
-        </>
+        <div className='flex justify-center'>
+        <div className='pt-8 w-3/4'>
+          <h1 className='text-xl font-bold'>{title}</h1>
+          <p>{summary}</p>
+        </div>
+        </div>
       ): null}
-    </>
+    </div>
   )
 }
 
